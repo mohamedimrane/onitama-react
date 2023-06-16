27141cf8bf68ea34b9d3ac9294704999428dc39f
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-06-16 22:34:52 +0100

    feat: :sparkles: Implements card generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import './App.css'
 import Board from "./Board"
+import { initialCards } from "./constants"
 
 function App() {
   const [board, setBoard] = useState([
@@ -11,11 +12,32 @@ function App() {
     ["s1", "s1", "m1", "s1", "s1"],
   ])
 
+  const [cards, setCards] = useState(generateCards(initialCards))
+
   return (
     <>
-      <Board board={board}/>
+      <Board board={board} />
     </>
   )
+}
+
+type Card = number[][]
+
+type CardDeck = Card[]
+
+type Cards = {
+  playerCards: CardDeck[];
+  fifthCard: Card
+}
+
+function generateCards(cards: CardDeck): Cards {
+  return {
+    playerCards: [
+      [cards[0], cards[1]],
+      [cards[2], cards[3]]
+    ],
+    fifthCard: cards[4]
+  }
 }
 
 export default App
