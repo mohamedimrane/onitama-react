70434b55343df729a8918192a6e1bf73e22ee733
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2023-06-16 22:18:14 +0100

    feat: :sparkles: Render board

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useState } from "react"
 import './App.css'
+import Board from "./Board"
 
 function App() {
+  const [board, setBoard] = useState([
+    ["s0", "s0", "m0", "s0", "s0"],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["", "", "", "", ""],
+    ["s1", "s1", "m1", "s1", "s1"],
+  ])
+
   return (
     <>
+      <Board board={board}/>
     </>
   )
 }
---
+++ src/Board.tsx
@@ -0,0 +1,19 @@
+import "./Board.css"
+
+type Props = {
+    board: string[][]
+}
+
+function Board({ board }: Props) {
+  return <>
+    <div className="board">
+      {board.map((row, iRow) => (<div className="row" key={iRow}>
+        {row.map((cell, iCell) => (<div className="cell" key={iCell}>
+          {cell}
+        </div>))}
+      </div>)) }
+    </div>
+  </>
+}
+
+export default Board
